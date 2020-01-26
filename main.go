80e3e4c3c9a98323c5371d7c50230cc44558a47c
Marc <muilpp@gmail.com>
2020-01-26 11:53:43 +0100

    Added a few R2 line stations

--- main.go
+++ main.go
@@ -8,19 +8,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// "79100" -> granollers centre
-// "71802" -> passeig de gràcia
-// http://localhost:8080/schedules/79100/71802
-
 func main() {
 	r := gin.Default()
 	r.Use(cors.Default())
 	r.GET("/schedules/:from/:to", func(c *gin.Context) {
-		from := c.Param("from")
-		to := c.Param("to")
 		limit, _ := strconv.Atoi(c.Query("limit"))
 
-		schedule := getNewTrainSchedule(from, to)
+		schedule := getNewTrainSchedule(c.Param("from"), c.Param("to"))
 		trainSchedules := parseTrainSchedules(schedule, limit)
 		c.JSON(http.StatusOK, trainSchedules)
 	})
